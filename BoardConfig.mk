c27907f1a4fedd88a098ae040235e74c75ebc5aa
Stricted <info@stricted.net>
2018-04-18 14:28:26 +0200

    initial commit
    Change-Id: Ib590e7b6630f8dae5de18f296ed4c7ef0a95f08f

---
+++ BoardConfig.mk
@@ -0,0 +1,56 @@
+# Bootloader
+TARGET_NO_BOOTLOADER := false
+TARGET_BOOTLOADER_BOARD_NAME := mt8127
+
+# Platform
+TARGET_BOARD_PLATFORM := mt8127
+
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a7
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Kernel
+#TARGET_PREBUILT_KERNEL := device/alcatel/ttab/kernel
+TARGET_KERNEL_CONFIG := ttab_defconfig
+TARGET_KERNEL_SOURCE := kernel/alcatel/ttab
+BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_CUSTOM_BOOTIMG_MK := device/alcatel/ttab/mkbootimg.mk
+BOARD_MKBOOTIMG_ARGS := --cmdline "$(BOARD_KERNEL_CMDLINE)" --base 0x80000000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --second_offset 0x00f00000 --tags_offset 0x00000100 --board vC1O-0
+
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
+
+TARGET_RECOVERY_FSTAB := device/alcatel/ttab/recovery/etc/twrp.fstab
+BOARD_HAS_MTK_HARDWARE := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# TWRP
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_NO_EXFAT := true
+TWHAVE_SELINUX := true
+TW_THEME := portrait_mdpi
+TW_NO_EXFAT_FUSE := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file"
+#TW_EXCLUDE_SUPERSU := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TWRP_EVENT_LOGGING := true
+TW_INCLUDE_CRYPTO := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_NO_SCREEN_BLANK := true
+
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
---
+++ omni_ttab.mk
@@ -0,0 +1,14 @@
+PRODUCT_RELEASE_NAME := ttab
+
+# Inherit device configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ttab
+PRODUCT_NAME := omni_ttab
+PRODUCT_BRAND := alcatel
+PRODUCT_MANUFACTURER := alcatel
+PRODUCT_MODEL := ttab
+
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 800
